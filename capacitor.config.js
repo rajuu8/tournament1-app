a6a0c85c72a7f23d80eded5696b7beded7a7b8ee
rajuu8 <r2499178@gmail.com>
2026-06-21 20:26:59 +0545

    Add files via upload

---
+++ capacitor.config.js
@@ -0,0 +1,15 @@
+const config = {
+  appId: 'com.rajuu8.battlezonearena',
+  appName: 'BattleZone Arena',
+  webDir: 'www',
+  server: {
+    url: 'https://rajuu8.github.io/tournament1-app/',
+    androidScheme: 'https',
+    cleartext: false
+  },
+  android: {
+    allowMixedContent: false
+  }
+};
+
+module.exports = config;
